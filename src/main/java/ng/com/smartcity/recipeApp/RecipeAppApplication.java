b1f9349db14e055da0561a75a701089d73f77133
onnamani <68425897+onnamani@users.noreply.github.com>
2022-03-24 10:25:44 +0100

    created a recipe app spring boot project

---
+++ src/main/java/ng/com/smartcity/recipeApp/RecipeAppApplication.java
@@ -0,0 +1,13 @@
+package ng.com.smartcity.recipeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeAppApplication.class, args);
+	}
+
+}
