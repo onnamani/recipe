8b5b768659f995c4a4cad1d621f81f8213cb7629
onnamani <68425897+onnamani@users.noreply.github.com>
2022-03-27 13:14:22 +0100

    rendering list of recipes on index view

--- src/main/java/ng/com/smartcity/recipeApp/services/RecipeServiceImpl.java
+++ src/main/java/ng/com/smartcity/recipeApp/services/RecipeServiceImpl.java
@@ -2,10 +2,12 @@ package ng.com.smartcity.recipeApp.services;
 
 import ng.com.smartcity.recipeApp.domain.Recipe;
 import ng.com.smartcity.recipeApp.repositories.RecipeRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
 public class RecipeServiceImpl implements RecipeService {
 
     private final RecipeRepository recipeRepository;
