3841473cb0c04fc3facefe5d823eed27fd29c5fa
onnamani <68425897+onnamani@users.noreply.github.com>
2022-04-01 17:03:48 +0100

    created integration test for UnitOfMeasureRepository

---
+++ src/test/java/ng/com/smartcity/recipeApp/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,29 @@
+package ng.com.smartcity.recipeApp.repositories;
+
+import ng.com.smartcity.recipeApp.domain.UnitOfMeasure;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Test
+    public void findByDescriptionTeaSpoon() throws Exception {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
+    }
+
+    @Test
+    public void findByDescriptionCup() throws Exception {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+        assertEquals("Cup", uomOptional.get().getDescription());
+    }
+}
