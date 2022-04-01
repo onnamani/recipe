2fca006210b1d6ce6fa8d8beea83c545b1e48257
onnamani <68425897+onnamani@users.noreply.github.com>
2022-04-01 13:21:32 +0100

    adding bootstrap css to index page

--- src/main/java/ng/com/smartcity/recipeApp/controllers/IndexController.java
+++ src/main/java/ng/com/smartcity/recipeApp/controllers/IndexController.java
@@ -1,14 +1,11 @@
 package ng.com.smartcity.recipeApp.controllers;
 
 import lombok.extern.slf4j.Slf4j;
-import ng.com.smartcity.recipeApp.domain.Category;
-import ng.com.smartcity.recipeApp.domain.UnitOfMeasure;
-import ng.com.smartcity.recipeApp.repositories.CategoryRepository;
-import ng.com.smartcity.recipeApp.repositories.UnitOfMeasureRepository;
 import ng.com.smartcity.recipeApp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Slf4j
 @Controller
