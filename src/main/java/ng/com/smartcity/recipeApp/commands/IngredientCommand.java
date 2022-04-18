96df1adf339e1fcc8fec4caf233d00c8fdff6e35
onnamani <68425897+onnamani@users.noreply.github.com>
2022-04-18 18:30:05 -0300

    created command objects of the data models

---
+++ src/main/java/ng/com/smartcity/recipeApp/commands/IngredientCommand.java
@@ -0,0 +1,18 @@
+package ng.com.smartcity.recipeApp.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class IngredientCommand {
+
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+    private UnitOfMeasureCommand unitOfMeasure;
+}
---
+++ src/main/java/ng/com/smartcity/recipeApp/commands/NotesCommand.java
@@ -0,0 +1,14 @@
+package ng.com.smartcity.recipeApp.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class NotesCommand {
+
+    private Long id;
+    private String recipeNotes;
+}
