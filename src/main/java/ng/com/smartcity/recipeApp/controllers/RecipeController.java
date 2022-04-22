93751ed7b67992d5fdf4e0338d83f8dd7d6e57ee
onnamani <68425897+onnamani@users.noreply.github.com>
2022-04-22 18:37:26 -0300

    refactoring getRecipeById url

--- src/main/java/ng/com/smartcity/recipeApp/controllers/RecipeController.java
+++ src/main/java/ng/com/smartcity/recipeApp/controllers/RecipeController.java
@@ -15,7 +15,7 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
+    @RequestMapping("/recipe/{id}/show")
     public String getRecipeById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
