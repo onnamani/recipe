668d5607986bb76c9eacb325691345209f862c0c
onnamani <68425897+onnamani@users.noreply.github.com>
2022-05-03 19:38:59 -0300

    creating a default image render when db recipe image is null

--- src/main/java/ng/com/smartcity/recipeApp/controllers/ImageController.java
+++ src/main/java/ng/com/smartcity/recipeApp/controllers/ImageController.java
@@ -1,5 +1,6 @@
 package ng.com.smartcity.recipeApp.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import ng.com.smartcity.recipeApp.commands.RecipeCommand;
 import ng.com.smartcity.recipeApp.services.ImageService;
 import ng.com.smartcity.recipeApp.services.RecipeService;
@@ -16,7 +17,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+@Slf4j
 @Controller
 public class ImageController {
 
@@ -39,11 +44,19 @@ public class ImageController {
     public void renderImageFromDB(@PathVariable String recipeId, HttpServletResponse response) throws IOException {
         RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(recipeId));
 
-        byte[] byteArray = new byte[recipeCommand.getImage().length];
+        byte[] byteArray;
 
-        int i = 0;
-        for(Byte e : recipeCommand.getImage())
-            byteArray[i++] = e;
+        Path defaultPath = Paths
+                .get("/Users/obinna/gitHubRepos/recipe/src/main/resources/static/images/imageplaceholder.jpeg");
+
+        if(recipeCommand.getImage() == null)
+            byteArray = Files.readAllBytes(defaultPath);
+        else {
+            byteArray = new byte[recipeCommand.getImage().length];
+            int i = 0;
+            for(Byte e : recipeCommand.getImage())
+                byteArray[i++] = e;
+        }
 
         response.setContentType("image/jpeg");
         InputStream is = new ByteArrayInputStream(byteArray);
